3ee42509fa631f833e8fcb8220ab9d18298ab646
starun <starun@ford.com>
2020-01-26 10:56:20 +0530

    Add project files.

---
+++ WebApp1/WebForm1.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //System.Threading.Thread.Sleep(10000);
+            string strTime = DateTime.Now.ToString();
+            Response.Write("Page cached at: " + strTime);
+        }
+    }
+}
